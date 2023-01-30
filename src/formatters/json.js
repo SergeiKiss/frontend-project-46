f5e1c726de4234fc64f09ba1b00c395a43881b53
Sergei Kiselev <kiselev.s.d.98@gmail.com>
2023-01-30 14:00:52 +0300

    fix plain formatter

--- src/formatters/json.js
+++ src/formatters/json.js
@@ -20,7 +20,7 @@ const isObject = (value, iter) => {
 export default (diff) => {
   const iter = (tree) => {
     const keys = Object.keys(tree);
-    const result = keys.reduce((arr, key, index) => {
+    const result = keys.reduce((acc, key, index) => {
       const splitedKey = key.split(' ');
       const sign = splitedKey[0];
       const actualKey = splitedKey[splitedKey.length - 1];
@@ -33,13 +33,13 @@ export default (diff) => {
       const previousSplitedKey = keys[index - 1] ? keys[index - 1].split(' ') : [];
       const previousActualKey = previousSplitedKey[previousSplitedKey.length - 1];
 
-      if (actualKey === previousActualKey) return arr;
+      if (actualKey === previousActualKey) return acc;
 
-      if (actualKey === nextActualKey) return [...arr, createUpdatedItem(actualKey, 'updated', isObject(value, iter), isObject(nextValue, iter))];
-      if (sign !== '+' && sign !== '-') return [...arr, createItem(actualKey, null, isObject(value, iter))];
-      if (sign === '-' && actualKey !== nextActualKey) return [...arr, createItem(actualKey, 'removed', isObject(value, iter))];
-      if (sign === '+' && actualKey !== nextActualKey) return [...arr, createItem(actualKey, 'added', isObject(value, iter))];
-      return null;
+      if (actualKey === nextActualKey) return [...acc, createUpdatedItem(actualKey, 'updated', isObject(value, iter), isObject(nextValue, iter))];
+      if (sign !== '+' && sign !== '-') return [...acc, createItem(actualKey, null, isObject(value, iter))];
+      if (sign === '-' && actualKey !== nextActualKey) return [...acc, createItem(actualKey, 'removed', isObject(value, iter))];
+      if (sign === '+' && actualKey !== nextActualKey) return [...acc, createItem(actualKey, 'added', isObject(value, iter))];
+      return acc;
     }, []);
     return result;
   };
